a44b968ca58eb5a3176ce7e1345ea353f25a3c09
Ryan Speers <ryan@riverloopsecurity.com>
2022-05-06 18:28:54 -0400

    Adding disclaimer required.

--- mtrace.cpp
+++ mtrace.cpp
@@ -1,6 +1,7 @@
 /*
  * Copyright River Loop Security 2020-2022 All Rights Reserved.
  * This material is based upon work supported by the Defense Advanced Research Projects Agency (DARPA) under Contract No. HR001119C0074.
+ * Any opinions, findings and conclusions or recommendations expressed in this material are those of the author(s) and do not necessarily reflect the views of the Defense Advanced Research Projects Agency (DARPA).
  */
 
 #include "pin.H"
